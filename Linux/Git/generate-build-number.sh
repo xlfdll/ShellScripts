dc79daa516d1ab8d73e24e435dac81bb0fd34892
xlfdll <xlfdll@gmail.com>
2020-02-07 23:12:57 -0800

    Updated Git build number generation script to support .NET Core projects

--- Linux/Git/generate-build-number.sh
+++ Linux/Git/generate-build-number.sh
@@ -8,19 +8,30 @@ commithash=`git show -s --format=%H`
 
 echo "Generating build $build ($builddate $buildtime)..."
 
-echo "// Auto-generated build information code file" > BuildInfo.cs
-echo "// Xlfdll Workstation" >> BuildInfo.cs
-echo "" >> BuildInfo.cs
-echo "namespace Xlfdll" >> BuildInfo.cs
-echo "    {" >> BuildInfo.cs
-echo "    internal static class BuildInfo" >> BuildInfo.cs
-echo "        {" >> BuildInfo.cs
-#echo "        internal const string Version = \"$buildversion\";" >> BuildInfo.cs
-echo "        internal const string Build = \"$build\";" >> BuildInfo.cs
-echo "        internal const string BuildDate = \"$builddate\";" >> BuildInfo.cs
-echo "        internal const string BuildTime = \"$buildtime\";" >> BuildInfo.cs
-echo "        internal const string CommitHash = \"$commithash\";" >> BuildInfo.cs
-echo "        }" >> BuildInfo.cs
-echo "    }" >> BuildInfo.cs
+# For .NET Framework projects
+# echo "// Auto-generated build information code file" > BuildInfo.cs
+# echo "// (C) Xlfdll Workstation" >> BuildInfo.cs
+# echo "" >> BuildInfo.cs
+# echo "namespace Xlfdll" >> BuildInfo.cs
+# echo "    {" >> BuildInfo.cs
+# echo "    internal static class BuildInfo" >> BuildInfo.cs
+# echo "        {" >> BuildInfo.cs
+# echo "        internal const string Version = \"$buildversion\";" >> BuildInfo.cs
+# echo "        internal const string Build = \"$build\";" >> BuildInfo.cs
+# echo "        internal const string BuildDate = \"$builddate\";" >> BuildInfo.cs
+# echo "        internal const string BuildTime = \"$buildtime\";" >> BuildInfo.cs
+# echo "        internal const string CommitHash = \"$commithash\";" >> BuildInfo.cs
+# echo "        }" >> BuildInfo.cs
+# echo "    }" >> BuildInfo.cs
+
+# For .NET Core projects
+# echo "<!-- Auto-generated build information code file -->" > Directory.Build.props
+# echo "<!-- (C) Xlfdll Workstation -->" >> Directory.Build.props
+# echo "" >> Directory.Build.props
+# echo "<Project>" >> Directory.Build.props
+# echo "  <PropertyGroup>" >> Directory.Build.props
+# echo "    <VersionSuffix>$build</VersionSuffix>" >> Directory.Build.props
+# echo "  </PropertyGroup>" >> Directory.Build.props
+# echo "</Project>" >> Directory.Build.props
 
 echo "Done."
