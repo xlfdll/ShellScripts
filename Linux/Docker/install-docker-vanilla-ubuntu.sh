cf2df38a8742b0e37c390c60b0392b95c2b7163f
xlfdll <xlfdll@gmail.com>
2020-04-13 08:35:35 -0700

    Added Docker installation script for Ubuntu

---
+++ Linux/Docker/install-docker-vanilla-ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install Docker Engine
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo docker run hello-world
+
+# Following https://docs.docker.com/engine/install/linux-postinstall/
+
+# Configure Docker for non-root user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+
+# Configure Docker autostart
+sudo systemctl enable docker
